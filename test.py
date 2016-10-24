9bdbdedce410d61966ac41d14f3afd343705e247
acalvano <arc2205@columbia.edu>
2016-10-24 18:18:10 -0400

    Corrected flake8 errors.

--- test.py
+++ test.py
@@ -97,6 +97,7 @@ class SubmitHintAnswerPairTest(AppTest):
 
         assert 'Submission successful' in response.data.decode()
 
+
 class CreatePuzzleTest(AppTest):
 
     def test_create_puzzle(self):
@@ -118,34 +119,40 @@ class CreatePuzzleTest(AppTest):
         assert 'Login successful' in response.data.decode()
 
         response = self.client.post('/create_puzzle', data=dict(
-                        hint_1="The movement of people from one place to another",
-                        answer_1="migration",
-                        hint_2="The number of deaths each year per 1,000 people",
-                        answer_2="deathrate",
-                        hint_3="Owners and workers who make products",
-                        answer_3="producers",
-                        hint_4="A government in which the king is limited by law",
-                        answer_4="constitutionalmonarchy",
-                        hint_5="the way a population is spread out over an area",
-                        answer_5="populationdistribution",
-                        hint_6="an economic system in which the central government controls owns factories, farms, and offices",
-                        answer_6="communism",
-                        hint_7="a form of government in which all adults take part in decisions",
-                        answer_7="directdemocracy",
-                        hint_8="people who move into one country from another",
-                        answer_8="immigrants",
-                        hint_9="nations with many industries and advanced technology",
-                        answer_9="developednations",
-                        hint_10="a king or queen inherits the throne by birth and has complete control",
-                        answer_10="absolutemonarchy",
-                        hint_11="the science that studies population distribution and change",
-                        answer_11="demography",
-                        hint_12="a region that belongs to another state",
-                        answer_12="dependency",
-                        hint_13="a set of laws that define and often limit a government's power",
-                        answer_13="constitution",
-                        hint_14="a system in which people make, exchange, and use things that have value",
-                        answer_14="economy",
+                hint_1="The movement of people from one place to another ",
+                answer_1="migration",
+                hint_2="The number of deaths each year per 1,000 people ",
+                answer_2="deathrate",
+                hint_3="Owners and workers who make products ",
+                answer_3="producers",
+                hint_4="A government in which the king is limited by law ",
+                answer_4="constitutionalmonarchy",
+                hint_5="the way a population is spread out over an area ",
+                answer_5="populationdistribution",
+                hint_6="an economic system in which the central government " +
+                       "controls owns factories, farms, and offices",
+                answer_6="communism",
+                hint_7="a form of government in which all adults take " +
+                       " part in decisions",
+                answer_7="directdemocracy",
+                hint_8="people who move into one country from another ",
+                answer_8="immigrants",
+                hint_9="nations with many industries and advanced technology ",
+                answer_9="developednations",
+                hint_10="a king or queen inherits the throne by birth " +
+                        "and has complete control",
+                answer_10="absolutemonarchy",
+                hint_11="the science that studies population " +
+                        "distribution and change",
+                answer_11="demography",
+                hint_12="a region that belongs to another state ",
+                answer_12="dependency",
+                hint_13="a set of laws that define and often limit a " +
+                        "government's power",
+                answer_13="constitution",
+                hint_14="a system in which people make, exchange, and use " +
+                        "things that have value",
+                answer_14="economy",
                     ), follow_redirects=True)
 
         assert 'Puzzle submitted successfully' in response.data.decode()
