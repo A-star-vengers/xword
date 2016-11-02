d6b6785dda94727ddb184e4c72da2699268e9493
acalvano <arc2205@columbia.edu>
2016-11-02 19:42:19 -0400

    Added tests for play_puzzle and browse_puzzles views.

--- tests/test_views.py
+++ tests/test_views.py
@@ -4,6 +4,7 @@ import unittest
 from flask_testing import TestCase
 from app import app
 from app.db import db, init_db
+from app.dbmodels import CrosswordPuzzle
 
 
 class AppTest(TestCase):
@@ -146,3 +147,90 @@ class CreatePuzzleTest(LoggedInAppTest):
 
         assert 'Puzzle submitted successfully' in response.data.decode()
 
+
+class PlayPuzzleTest(LoggedInAppTest):
+
+    def create_puzzle(self):
+
+        response = self.client.post('/create_puzzle', data=dict(
+                title="Geography Questions",
+                hint_1="The movement of people from one place to another ",
+                answer_1="migration",
+                hint_2="The number of deaths each year per 1,000 people ",
+                answer_2="deathrate",
+                hint_3="Owners and workers who make products ",
+                answer_3="producers",
+                hint_4="A government in which the king is limited by law ",
+                answer_4="constitutionalmonarchy",
+                hint_5="the way a population is spread out over an area ",
+                answer_5="populationdistribution",
+                hint_6="an economic system in which the central government " +
+                       "controls owns factories, farms, and offices",
+                answer_6="communism",
+                hint_7="a form of government in which all adults take " +
+                       " part in decisions",
+                answer_7="directdemocracy",
+                hint_8="people who move into one country from another ",
+                answer_8="immigrants",
+                hint_9="nations with many industries and advanced technology ",
+                answer_9="developednations",
+                hint_10="a king or queen inherits the throne by birth " +
+                        "and has complete control",
+                answer_10="absolutemonarchy",
+                hint_11="the science that studies population " +
+                        "distribution and change",
+                answer_11="demography",
+                hint_12="a region that belongs to another state ",
+                answer_12="dependency",
+                hint_13="a set of laws that define and often limit a " +
+                        "government's power",
+                answer_13="constitution",
+                hint_14="a system in which people make, exchange, and use " +
+                        "things that have value",
+                answer_14="economy",
+                    ), follow_redirects=True)
+
+
+    def test_no_puzzle(self):
+
+        response = self.client.get('/play_puzzle', follow_redirects=True)
+
+        assert 'No puzzles yet' in response.data.decode()
+
+    def test_with_puzzle(self):
+
+        self.create_puzzle()
+
+        response = self.client.get('/play_puzzle', follow_redirects=True)
+
+        assert "puzzleData" in response.data.decode()
+
+
+class BrowsePuzzleTest(LoggedInAppTest):
+
+    def fill_fake_puzzle_db(self):
+
+        title = "Puzzle"
+
+        for x in range(100):
+            puzzle = CrosswordPuzzle(10, 25, 25, title + str(x))
+            db.session.add(puzzle)
+            db.session.commit()
+
+    def test_browse_no_puzzles(self):
+
+        response = self.client.get('/browse_puzzles', follow_redirects=True)
+
+        assert "thumbnail" not in response.data.decode()
+
+    def test_browse_puzzles(self):
+
+        self.fill_fake_puzzle_db()
+
+        response = self.client.get('/browse_puzzles', follow_redirects=True)
+
+        assert "Next" in response.data.decode()
+
+        response = self.client.get('/browse_puzzles/page/2', follow_redirects=True)
+
+        assert "Next" in response.data.decode() and "Prev" in response.data.decode()
