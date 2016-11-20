bad04e87ed7abbe0bad3dcbadf32c4be7cffb786
acalvano <arc2205@columbia.edu>
2016-11-20 12:37:09 -0500

    Started work on theme integration to submit hint/answer pair page.

---
+++ app/static/js/submit_hint.js
@@ -0,0 +1,28 @@
+$(function()
+{
+    $(document).on('click', '.btn-add', function(e)
+    {
+        e.preventDefault();
+
+        var tbody = $(this).parents('tr').parents('tbody');
+
+        var currentEntry = $(this).parents('tr');
+
+        var newEntry = $(currentEntry.clone()).appendTo(tbody);
+
+        var newLength = tbody[0].children.length;
+
+        newEntry.find('input').val('');
+
+        newEntry.find('input[name^=theme]').attr('name', 'theme' + newLength);
+
+        currentEntry.find('.btn-add').removeClass('btn-add')
+                                     .addClass('btn-remove')
+                                     .addClass('btn-danger')
+                    .html('<span class="glyphicon glyphicon-minus"></span>');
+    }).
+    on('click', '.btn-remove', function(e)
+    {
+        $(this).parents('tr').remove();
+    });
+});
