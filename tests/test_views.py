bc8d6cd92fade0e3ae414c37066737ef9bbf0835
Bruno Martins <brunoseivam@gmail.com>
2016-12-08 21:23:50 -0500

    Fix test_puzzle_creator_renders

--- tests/test_views.py
+++ tests/test_views.py
@@ -93,6 +93,46 @@ class LoggedInAppTestWithFilledQuestionDb(LoggedInAppTest):
                 answer_14="economy"
                     ), follow_redirects=True)
 
+        self.client.post('/create_puzzle', data=dict(
+            title='Geography',
+            num_rows=25,
+            num_cols=25,
+            hint_1="The movement of people from one place to another ",
+            answer_1="migration",
+            hint_2="The number of deaths each year per 1,000 people ",
+            answer_2="deathrate",
+            hint_3="Owners and workers who make products ",
+            answer_3="producers",
+            hint_4="A government in which the king is limited by law ",
+            answer_4="constitutionalmonarchy",
+            hint_5="the way a population is spread out over an area ",
+            answer_5="populationdistribution",
+            hint_6="an economic system in which the central government " +
+                   "controls owns factories, farms, and offices",
+            answer_6="communism",
+            hint_7="a form of government in which all adults take " +
+                   " part in decisions",
+            answer_7="directdemocracy",
+            hint_8="people who move into one country from another ",
+            answer_8="immigrants",
+            hint_9="nations with many industries and advanced technology ",
+            answer_9="developednations",
+            hint_10="a king or queen inherits the throne by birth " +
+                    "and has complete control",
+            answer_10="absolutemonarchy",
+            hint_11="the science that studies population " +
+                    "distribution and change",
+            answer_11="demography",
+            hint_12="a region that belongs to another state ",
+            answer_12="dependency",
+            hint_13="a set of laws that define and often limit a " +
+                    "government's power",
+            answer_13="constitution",
+            hint_14="a system in which people make, exchange, and use " +
+                    "things that have value",
+            answer_14="economy"
+        ), follow_redirects=True)
+
 
 class LoginTest(AppTest):
 
